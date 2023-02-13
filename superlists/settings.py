ca31a1a4fb53dbcacf6ca49b45975bca44177693
davidalexander83 <davidalexander83@icloud.com>
2023-02-13 16:58:38 +1100

    add allowed hosts and turn off debug

--- superlists/settings.py
+++ superlists/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-fvs*awv^p+rx^ryg!t7slja4%e)de=u#q(6%x+)^zbocva=ktg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ubuntustaging.superlists.example']
 
